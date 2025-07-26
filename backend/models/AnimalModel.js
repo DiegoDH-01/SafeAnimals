f63f7eae9cfb506b39334c030dce6fe4867fe348
DiegoDH <108910778+DiegoDH-01@users.noreply.github.com>
2025-07-26 14:08:32 -0500

    Feature Login Back y Fornt

--- backend/models/AnimalModel.js
+++
@@ -1,9 +0,0 @@
-// backend/models/AnimalModel.js
-const db = require('./db');
-
-const getAllAnimals = async () => {
-  const [rows] = await db.query('SELECT * FROM dueno');
-  return rows;
-};
-
-module.exports = { getAllAnimals };
